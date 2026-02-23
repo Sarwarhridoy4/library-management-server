807a6224f8f3eeba9b0c7b7ed8e638986d2ff069
Sarwar hossain <sarwarhridoy4@gmail.com>
2026-02-23 14:47:25 +0600

    Fix Mongoose filter type

--- src/app/modules/book/book.service.ts
+++ src/app/modules/book/book.service.ts
@@ -1,7 +1,7 @@
 // src/modules/book/book.service.ts
 import { IBookBase, IBookDocument } from "./book.interface";
 import { Book } from "./book.model";
-import { FilterQuery } from "mongoose";
+import type { QueryFilter } from "mongoose";
 import { IBookQuery } from "./book.types";
 
 export const createBook = async (payload: IBookBase) => {
@@ -10,7 +10,7 @@ export const createBook = async (payload: IBookBase) => {
 };
 
 export const getBooks = async (query: IBookQuery) => {
-  const filter: FilterQuery<IBookDocument> = {};
+  const filter: QueryFilter<IBookDocument> = {};
   if (query.filter) filter.genre = query.filter;
 
   const sortBy = query.sortBy || "createdAt";
